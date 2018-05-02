4526398c25d9a5dd6256f4f5b83c06782e36ddcc
lijun <30607580@qq.com>
2018-05-02 19:21:06 +0800

    组件内部钩子beforeRouteEnter/beforRouteUpdata/beforRouteLeave

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,8 +16,22 @@
       }
     },
     beforeRouteEnter(to, from, next){
-      console.log('通过组件内部判断到当前访问Home页面，放行、通过！')
+      //console.log('通过组件内部判断到当前访问Home页面，放行、通过！')
+      //next();
+      //问题：项目场景，在执行beforeRouteEnter后改变数据data里面值？
+      //重点：beforeRouteEnter钩子比beforCreate都要更早执行，因此我们是不能通过this.msg拿到当前deta值
+      //解决方案：next回调函数
+      next((vm)=>{
+          vm.msg = "欢迎访问我的路由案例，我是在beforeRouteEnter执行后改变过的数据！"
+      })
+    },
+    beforRouteUpdata(to, from, next){
       next();
+      //这个钩子函数主要是针对主导航下的二级导航，也就是说我们之前那么钩子函数都是针对主导航来进行设置，这个是针对二级导航
+    },
+    beforRouteLeave(to, from, next){
+      next();
+      //离开时时候执行钩子函数，如果设置next(false)，那么组件就一直停留在当前路由组件，无法进入其他任何路由组件
     }
   }
 </script>
