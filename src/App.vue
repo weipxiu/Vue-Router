18a98d4229fb078e6139ea7aa46e9e2bcf6861eb
lijun <lijun@021.com>
2018-10-30 09:56:20 +0800

    更新代码

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
   <div id="app">
     <ul class="nav">
       <!-- <li><router-link to='/home'>我是首页</router-link></li> -->
-      <li><router-link to='/' exact tag="span" event='mouseover' active-class='span-active'><i></i>Home</router-link></li>
-      <li><router-link to='/about' event='mouseover'>About</router-link></li>    
-      <li><router-link :to="{path:'/document'}" exact event='mouseover'>Document</router-link></li>    
-      <li><router-link :to="{path:'/leaving'}" exact event='mouseover'>Leaving</router-link></li>    
-      <li><router-link to='/undefined' exact event='mouseover'>Undefined</router-link></li>    
-      <li><router-link to='/user' event='mouseover'>User</router-link></li>    
+      <li>
+        <router-link to='/' exact tag="span" event='mouseover' active-class='span-active'><i></i>Home</router-link>
+      </li>
+      <li>
+        <router-link to='/about' event='mouseover'>About</router-link>
+      </li>
+      <li>
+        <router-link :to="{path:'/document'}" exact event='mouseover'>Document</router-link>
+      </li>
+      <li>
+        <router-link :to="{path:'/leaving'}" exact event='mouseover'>Leaving</router-link>
+      </li>
+      <li>
+        <router-link to='/undefined' exact event='mouseover'>Undefined</router-link>
+      </li>
+      <li>
+        <router-link to='/user' event='mouseover'>User</router-link>
+      </li>
     </ul>
 
     <transition>
-    <router-view name="slide"></router-view>
+      <router-view name="slide"></router-view>
     </transition>
 
     <!-- 运动模式out-in/in-out -->
@@ -22,105 +34,108 @@
     <transition name="left">
       <router-view class="router-view"></router-view>
     </transition>
-    
+
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
 export default {
-  data(){
+  data() {
     return {
-      index : '/home'
+      index: '/home'
     }
   },
   name: 'App',
-  components:{
+  components: {
     HelloWorld //注册组件名称首字母大写
   }
 }
 </script>
 
 <style>
-*{
-  margin:0;
+* {
+  margin: 0;
   padding: 0;
 }
-h1,h2{
-  margin:25px 0;
+h1,
+h2 {
+  margin: 25px 0;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
-.nav{
-  margin:30px 0;
+.nav {
+  margin: 30px 0;
 }
-ul.nav li{
+ul.nav li {
   display: inline-block;
 }
-ul.nav li a,ul.nav li span{
-  font-size:14px;
-  color:#fff;
-  padding:5px 10px;
+ul.nav li a,
+ul.nav li span {
+  font-size: 14px;
+  color: #fff;
+  padding: 5px 10px;
   border-radius: 2px;
   background: rgb(156, 238, 63);
-  text-decoration: none
+  text-decoration: none;
 }
-ul.nav li span.span-active{
-  background: #e62a91
+ul.nav li span.span-active {
+  background: #e62a91;
 }
-ul.nav .action{
-  background: rgb(38, 219, 113)
+ul.nav .action {
+  background: rgb(38, 219, 113);
 }
-.router-view{
+.router-view {
   width: 100%;
   position: absolute;
-  left:0;
-  top:100px
+  left: 0;
+  top: 100px;
 }
 /*路由组件进出场动画效果start*/
-.v-enter{
+.v-enter {
   opacity: 0;
 }
-.v-enter-to{
+.v-enter-to {
   opacity: 1;
 }
-.v-enter-active{
-  transition: 1s
+.v-enter-active {
+  transition: 1s;
 }
-.v-leave{
+.v-leave {
   opacity: 1;
 }
-.v-leave-to{
+.v-leave-to {
   opacity: 0;
 }
-.v-leave-active{
-  transition:0.7s
+.v-leave-active {
+  transition: 0.7s;
 }
 /*路由组件进出场动画效果end*/
 
 /*自定义的组件动画方式start*/
 /*注意，在自定义动画时候的class只能修改v-，后面的不可以修改*/
-.left-enter{ 
+.left-enter {
   transform: translateX(100%);
 }
-.left-enter-to{ 
-  transform: translateX(0%)
+.left-enter-to {
+  transform: translateX(0%);
 }
-.left-enter-active,.left-leave-active{ 
-  transition: 1s
+.left-enter-active,
+.left-leave-active {
+  transition: 1s;
 }
-.left-leave{ 
+.left-leave {
   transform: translateX(0);
 }
-.left-leave-to{ 
-  transform: translateX(-100%)
+.left-leave-to {
+  transform: translateX(-100%);
 }
 /*自定义的组件动画方式end*/
 </style>
--- src/components/user.vue
+++ src/components/user.vue
@@ -6,7 +6,8 @@
     <ul class="nav">
       <router-link :to="'/user/'+item.id" v-for="item in dataList" :key="item.id" tag="li">{{item.userName}}</router-link>
     </ul>
-    <div v-if="userInfo.id"> <!--判断里面对象有木有，如果木有就不渲染了-->
+    <div v-if="userInfo.id">
+      <!--判断里面对象有木有，如果木有就不渲染了-->
       <p>姓名：{{userInfo.userName}}</p>
       <p>性别：{{userInfo.sex}}</p>
       <p>爱好：{{userInfo.hobby}}</p>
@@ -14,47 +15,48 @@
   </div>
 </template>
 <script>
-  import userData  from '@/data.js'
-  export default {
-    name: 'user',
-    data () {
-      return {
-        dataList: userData,
-        userInfo:{}
-      }
-    },
-    watch:{
-      "$route":function(){
+import userData from '@/data.js'
+export default {
+  name: 'user',
+  data() {
+    return {
+      dataList: userData,
+      userInfo: {}
+    }
+  },
+  watch: {
+    "$route": function () {
       this.wacchList()
+    }
+  },
+  created() {
+    /*created只会在初始化时候执行一次，所以我们要将里面的代码抽离出来，
+    在watch里监听路由变化来执行相同代码做相同的事*/
+    this.wacchList()
+  },
+  methods: {
+    wacchList() {
+      let id = this.$route.params.xxx;
+      console.log(this.$route)//可以拿到当前路由对象的所有属性、参数
+      /*id：拿到动态路由$route.params对象信息,
+      注意，是$route而不是$router，xxx是在路由index.js文件里配置的自定义参数*/
+      if (!id) {
+        this.userInfo = {}
+        return
       }
-    },
-    created(){
-      /*created只会在初始化时候执行一次，所以我们要将里面的代码抽离出来，
-      在watch里监听路由变化来执行相同代码做相同的事*/
-      this.wacchList()
-    },
-    methods:{
-      wacchList(){
-        let id = this.$route.params.xxx;
-        console.log(this.$route)//可以拿到当前路由对象的所有属性、参数
-        /*id：拿到动态路由$route.params对象信息,
-        注意，是$route而不是$router，xxx是在路由index.js文件里配置的自定义参数*/
-        if(!id){
-          this.userInfo = {}
-          return
-        }
-        this.userInfo = this.dataList.filter((item)=>{
-          return item.id == id;
-        })[0] //过滤后的数组
-        console.log(this.userInfo)
-      }
+      this.userInfo = this.dataList.filter((item) => {
+        return item.id == id;
+      })[0] //过滤后的数组
+      console.log(this.userInfo)
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -68,19 +70,19 @@ li {
 a {
   color: #42b983;
 }
-p{
-  margin-top:25px;
+p {
+  margin-top: 25px;
 }
-ul.nav li{
-  font-size:14px;
-  color:#fff;
-  padding:5px 10px;
+ul.nav li {
+  font-size: 14px;
+  color: #fff;
+  padding: 5px 10px;
   border-radius: 2px;
   background: rgb(156, 238, 63);
   text-decoration: none;
   cursor: pointer;
 }
-ul.nav .action{
-  background: rgb(38, 219, 113)
+ul.nav .action {
+  background: rgb(38, 219, 113);
 }
 </style>
